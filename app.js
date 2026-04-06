a67a04ad3f78295b1c2cd871dbc2409c998eb7ff
Jean Costa <jc090504@gmail.com>
2026-04-06 09:32:50 -0300

    Adicionando arquivos

--- app.js
+++ app.js
@@ -2,3 +2,30 @@
 
 import funcionarios from "./funcionarios.json" with { type: "json" }
 
+//Transformar os dados do JSON em elementos HTML para exibir na página
+function criarCard(funcionario) {
+    const card = document.createElement("div")
+    card.classList.add("card")
+
+    const foto = document.createElement("img")
+    foto.src = `./img/${funcionario.imagem}`
+    foto.alt = `Foto de ${funcionario.nome}`
+
+    const nome = document.createElement("h3")
+    nome.textContent = funcionario.nome
+
+    const cargo = document.createElement("span")
+    cargo.textContent = `Cargo: ${funcionario.cargo}`
+
+    card.append(foto, nome, cargo)
+
+    return card
+}
+
+const cards = funcionarios.map(criarCard)
+
+document.getElementById('container').replaceChildren(...cards)
+
+
+
+
